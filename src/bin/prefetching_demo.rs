c29050d35f3eeb7ec630556b5ed831888416ba27
mbulhakov <bulhakov.maksym@gmail.com>
2022-08-28 00:30:44 +0300

    Minor improvements

--- src/bin/prefetching_demo.rs
+++ src/bin/prefetching_demo.rs
@@ -1,8 +1,11 @@
 use krusty::prefetch;
+use std::boxed::*;
+use std::error::Error;
 
-fn main() {
-    let data = prefetch::mp3();
+fn main() -> Result<(), Box<dyn Error>> {
+    let data = prefetch::mp3()?;
     for key in data.keys() {
         println!("{}", key);
     }
+    Ok(())
 }
--- src/prefetch.rs
+++ src/prefetch.rs
@@ -1,43 +1,50 @@
+use std::boxed::*;
 use std::collections::HashMap;
+use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::Cursor;
 use zip::read::*;
 
 fn download_archive() -> Result<Vec<u8>, std::io::Error> {
-    let mut f = File::open(r"GSP.zip")?;
+    let mut f = File::open(r"C:\Users\Maksym\Documents\GSP.zip")?;
     let mut buffer = Vec::new();
     f.read_to_end(&mut buffer)?;
 
     Ok(buffer)
 }
 
-pub fn mp3() -> HashMap<String, Vec<u8>> {
-    let mut files = HashMap::new();
+pub fn mp3() -> Result<HashMap<String, Vec<u8>>, Box<dyn Error>> {
+    let mut result = HashMap::new();
 
-    let mut zip_data = Vec::new();
-    match download_archive() {
-        Ok(data) => zip_data = data,
-        Err(_) => return HashMap::new(),
-    }
-
-    let mut archive = ZipArchive::new(Cursor::new(zip_data)).unwrap();
+    let mut archive = ZipArchive::new(Cursor::new(download_archive()?))?;
     for i in 0..archive.len() {
-        let mut file = archive.by_index(i).unwrap();
-        let name = match file.enclosed_name() {
-            Some(path) => path.to_owned(),
+        let mut file = archive.by_index(i)?;
+        if file.is_dir() {
+            continue;
+        }
+
+        let name = file
+            .enclosed_name()
+            .and_then(|x| x.to_str())
+            .and_then(|x| Some(x.to_string()));
+
+        let name = match name {
             None => continue,
+            Some(n) => n,
         };
 
         let mut buffer = Vec::new();
-
         match std::io::copy(&mut file, &mut buffer) {
             Ok(_) => (),
-            Err(_) => continue,
+            Err(err) => {
+                log::error!("Failed to copy '{}': {}", name, err);
+                continue;
+            }
         }
 
-        files.insert(name.to_str().unwrap().to_string(), buffer);
+        result.insert(name, buffer);
     }
 
-    return files;
+    return Ok(result);
 }
