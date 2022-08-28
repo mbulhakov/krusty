c4e605dafe10488dc7e0b3e3bd4291c72465522c
mbulhakov <bulhakov.maksym@gmail.com>
2022-08-28 19:29:48 +0300

    Implemented download from Drive

--- src/bin/prefetching_demo.rs
+++ src/bin/prefetching_demo.rs
@@ -1,9 +1,10 @@
-use krusty::prefetch;
-use std::boxed::*;
+use krusty::prefetch::gachi::mp3;
+use std::boxed::Box;
 use std::error::Error;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let data = prefetch::mp3()?;
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let data = mp3().await?;
     for key in data.keys() {
         println!("{}", key);
     }
