53e46f7e8a97cbf732e6b9d4fa5d357181c916c4
mbulhakov <bulhakov.maksym@gmail.com>
2022-08-29 20:02:25 +0300

    Minor fix

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ enum Command {
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
-    log::info!("Starting throw dice bot...");
+    log::info!("Starting bot...");
 
     let bot = Bot::from_env().auto_send();
 
